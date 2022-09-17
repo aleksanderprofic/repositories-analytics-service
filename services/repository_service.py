553c4c7f35d7d192b0bf570f33f8e00e499295a9
Aleksander Profic <aleksanderp@qualtrics.com>
2022-09-17 23:23:58 +0200

    Delete some files

--- services/repository_service.py
+++
@@ -1,6 +0,0 @@
-from clients import github
-
-
-def get_repository(repository_name: str):
-    # In the future: add logic to decide which provider to use (GitHub, Gitlab, Bitbucket etc.)
-    return github.get_repository(repository_name)
